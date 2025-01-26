69cf50c0b4b0561d29f58a83bd93012be57297e5
pglazkov <glazprom@mail.ru>
2025-01-26 20:27:01 +0300

    feat: добавил валидацию и описал методы хендлера

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"fmt"
 	"go/link-shorter/configs"
+	"go/link-shorter/pkg/request"
 	"go/link-shorter/pkg/response"
 	"net/http"
 )
@@ -24,20 +25,26 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 
 func (handler *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-
-		fmt.Println(handler.Config.Auth.Secret)
+		body, err := request.HandleBody[LoginRequest](&w, req)
+		if err != nil {
+			return
+		}
+		fmt.Println(body)
 		fmt.Println("Hello World!")
 
 		data := LoginResponse{
 			Token: "123",
 		}
-
 		response.Json(w, data, http.StatusOK)
 	}
 }
 
 func (handler *AuthHandler) Register() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		fmt.Println("Hello World!")
+		body, err := request.HandleBody[RegisterRequest](&w, req)
+		if err != nil {
+			return
+		}
+		fmt.Println(body)
 	}
 }
